d247106c890fddbcafb2e1bf960661c5ebe97ace
mayu-watanabe <mayu.watanabe.1994@gmail.com>
2023-03-26 10:37:11 +0900

    add text, fix design

--- src/pages/vnente.js
+++ src/pages/vnente.js
@@ -16,20 +16,30 @@ const SecondPage = () => (
         className="img-fluid"
       />
       <h2 className="color-title py-4">_vnente</h2>
-      <p className="my-1 pb-5">The shaping of an oval form through an empirical / alchemical approach .</p>
-      <p className="my-1">The experimenting while looking for the minimum balance of the base of the human</p>
-      <p className="my-1">figure , in movement , has led to the carving out of the negative egg and the subsequent cut</p>
-      <p className="my-1">of the two halves, relating to the anatomical detail of the arch of the foot .</p>
-      <p className="my-1">The gesture has led to the definition of a form that now seems to be calling to be handled ,</p>
-      <p className="my-1">twisted within its irregular split axes , while interrupting the magnetic forces holding together</p>
-      <p className="my-1 pb-5">the two halves .</p>
-      <p className="my-1">Thus revealing the third ear within .</p>
-      <p className="my-1">The third ear stands alone aside .</p>
-      <p className="my-1">The third ear is held as a complementary form within the partial egg .</p>
-      <p className="my-1">The egg might be magnetically attracted to its complementary , partial , hidden form , at the</p>
-      <p className="my-1">diaphragm’s height .</p>
-      <p className="my-1">Symbolically closer to the diaphragm , its inner sound .</p>
-      <p className="my-1 pb-5">To be auscultated .</p>
+      <p className="my-0 pb-5">The shaping of an oval form through an empirical / alchemical approach .</p>
+      <p className="my-0">The experimenting while looking for the minimum balance of the base of the human</p>
+      <p className="my-0">figure , in movement , has led to the carving out of the negative egg and the subsequent cut</p>
+      <p className="my-0">of the two halves, relating to the anatomical detail of the arch of the foot .</p>
+      <p className="my-0">The gesture has led to the definition of a form that now seems to be calling to be handled ,</p>
+      <p className="my-0">twisted within its irregular split axes , while interrupting the magnetic forces holding together</p>
+      <p className="my-0 pb-4">the two halves .</p>
+      <div className="text-end py-4">
+        <StaticImage
+          src="../images/DSC08494_BW.png"
+          width={800}
+          quality={95}
+          formats={["AUTO", "WEBP"]}
+          alt="DSC08494_BW"
+          className="img-fluid"
+        />
+      </div>
+      <p className="my-0 pt-4">Thus revealing the third ear within .</p>
+      <p className="my-0">The third ear stands alone aside .</p>
+      <p className="my-0">The third ear is held as a complementary form within the partial egg .</p>
+      <p className="my-0">The egg might be magnetically attracted to its complementary , partial , hidden form , at the</p>
+      <p className="my-0">diaphragm’s height .</p>
+      <p className="my-0">Symbolically closer to the diaphragm , its inner sound .</p>
+      <p className="my-0 pb-5">To be auscultated .</p>
     </div>
   </Layout>
 )
--- src/pages/vnospinari0.js
+++ src/pages/vnospinari0.js
@@ -16,19 +16,27 @@ const SecondPage = () => (
         className="img-fluid"
       />
       <h2 className="color-title py-4">_vnospinari0</h2>
-      <p className="my-1">texttexttexttexttext</p>
-      <p className="my-1">texttexttexttexttext</p>
-      <p className="my-1">texttexttexttexttext</p>
-      <p className="my-1">texttexttexttexttext</p>
-      <p className="my-1">texttexttexttexttext</p>
-      <StaticImage
-        src="../images/horizontal_compisition.png"
-        layout="fullWidth"
-        quality={95}
-        formats={["AUTO", "WEBP"]}
-        alt="A Gatsby astronaut"
-        className="img-fluid"
-      />
+      <p className="my-0">a chain - begins / ends</p>
+      <p className="my-0">a body as its closure</p>
+      <p className="my-0 pb-5">a bridge linking selves</p>
+      <p className="my-0">the primary shapes</p>
+      <p className="my-0">circle _ triangle then square</p>
+      <p className="my-0">square _ triangle then circle</p>
+      <p className="my-0 pb-5">the twisted ladder</p>
+      <p className="my-0">thorns resemble calls</p>
+      <p className="my-0">calls resemble wounds</p>
+      <p className="my-0">wounds resemble rhythms</p>
+      <p className="my-0 pb-5">rhythms resemble thorns</p>
+      <div className="pb-5">
+        <StaticImage
+          src="../images/horizontal_compisition.png"
+          layout="fullWidth"
+          quality={95}
+          formats={["AUTO", "WEBP"]}
+          alt="A Gatsby astronaut"
+          className="img-fluid"
+        />
+      </div>
     </div>
   </Layout>
 )
