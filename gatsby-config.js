0786f9c3cd1f002fec4707bf652bf5e2655b3468
mayu-watanabe <mayu.watanabe.1994@gmail.com>
2023-04-29 11:30:22 +0900

    import Apercu Mono font

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,5 +37,45 @@ module.exports = {
     },
     `gatsby-plugin-gatsby-cloud`,
     `gatsby-plugin-transition-link`,
+    {
+      resolve: `gatsby-plugin-webfonts`,
+      options: {
+        fonts: {
+          // google: [
+          //   {
+          //     family: "Roboto", // 'font-family' property
+          //     variants: ["300", "400", "500"],
+          //     //subsets: ['latin'],
+          //     //text: 'Hello',
+          //     //fontDisplay: 'swap' || 'auto' || 'block' || 'fallback' || 'optional',
+          //     //strategy: 'selfHosted' || 'base64' || 'cdn',
+          //     // Other properties as per https://developer.mozilla.org/en-US/docs/Web/CSS/@font-face (except 'src' & 'font-family') can go here i.e.
+          //     //[cssProperty]: 'value',
+          //   },
+          // ],
+          selfHosted: [
+            {
+              family: "Apercu Mono",
+              urls: {
+                woff2: `/src/fonts/Apercu-Mono.woff2`,
+                woff: `/src/fonts/Apercu-Mono.woff`,
+                //[format]: '/[filepath]/[filename],
+              },
+              //[cssProperty]: 'value',
+            },
+          ],
+        },
+        // formatAgents: {
+        //   eot: `Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET4.0C; .NET4.0E)`,
+        //   ttf: `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/534.59.8 (KHTML, like Gecko) Version/5.1.9 Safari/534.59.8`,
+        //   woff: `Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; .NET4.0C; .NET4.0E; .NET CLR 2.0.50727; .NET CLR 3.0.30729; .NET CLR 3.5.30729; rv:11.0) like Gecko`,
+        //   woff2: `Mozilla/5.0 (Windows NT 10.0; Win64; x64; ServiceUI 8) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393`,
+        // },
+        //formats: ['woff2', 'woff', 'otf', 'ttf'],
+        //useMinify: true,
+        //usePreload: true,
+        //usePreconnect: false,
+      },
+    },
   ],
 }
