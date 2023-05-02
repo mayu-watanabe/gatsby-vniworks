3acaa208c43e0d0c6a982defdce9162a521def70
mayu-watanabe <mayu.watanabe.1994@gmail.com>
2023-05-02 18:00:21 +0900

    fix build error

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,13 +6,15 @@ import Header from "./header"
 import "./../css/layout.scss"
 
 const Layout = ({ children }) => {  
-  window.addEventListener('mousewheel', e => {
-    let imageArea = document.getElementById('image-area');
-    if (e.deltaX === -0 && e.target.tagName === 'IMG') {
-      imageArea.scrollBy(e.deltaY,0);
-      e.preventDefault();
-    }
-  }, { passive: false })
+  if (typeof window !== 'undefined') { 
+    window.addEventListener('mousewheel', e => {
+      let imageArea = document.getElementById('image-area');
+      if (e.deltaX === -0 && e.target.tagName === 'IMG') {
+        imageArea.scrollBy(e.deltaY,0);
+        e.preventDefault();
+      }
+    }, { passive: false })
+  }
 
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
